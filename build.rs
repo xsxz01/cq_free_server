07edf51c7dddb14df4bc4b23c31ec9123401295d
Pang <pangyuyu@illusiontech.cn>
2025-02-25 10:04:03 +0800

    chore: 忽略 node_modules 目录
    - 在 .gitignore 文件中添加 node_modules 目录，使其被 Git 忽略

--- build.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    let config = slint_build::CompilerConfiguration::new()
-        .with_style("fluent".into());
-    slint_build::compile_with_config("ui/app-window.slint", config).expect("Slint build failed");
-}
